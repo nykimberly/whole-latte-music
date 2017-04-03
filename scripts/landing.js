8d2d3ee48db8e1caf473dc10df860350d2ead14f
Kimberly Nguyen <kimberlyvnguyen@gmail.com>
2017-04-02 18:55:59 -0700

    assignment-9-events: completed... deleted animatePoints fn by accident. added animatePoints back in.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,29 +1,30 @@
 (function () {
     "use strict";
     
-    var points, i, revealPoint, animatePoints, sellingPoints, scrollDistance;
+    var points, i, forEach, revealPoint, animatePoints, sellingPoints, scrollDistance;
     
     points = document.getElementsByClassName('point');
-    
-    revealPoint = function (i) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+
+    animatePoints = function (points) {
+    //forEach function is in utilities folder!
+        //function forEach(array, callback) {
+        //    for (i = 0; i < array.length; i += 1) {
+        //        callback(i);
+        //    }
+        //}
+        forEach(points, revealPoint = function (i) {
+            points[i].style.opacity = 1;
+            points[i].style.transform = "scaleX(1) translateY(0)";
+            points[i].style.msTransform = "scaleX(1) translateY(0)";
+            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+        });
     };
-    
-    function forEach(array, callback) {
-        for (i = 0; i < array.length; i += 1) {
-            callback(i);
-        }
-    }
-    
-    forEach(points, revealPoint);
-    
+
     window.onload = function () {
         if (window.innerHeight > 950) {
             animatePoints(points);
         }
+        
         sellingPoints = document.getElementsByClassName('selling-points')[0];
         
         scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
