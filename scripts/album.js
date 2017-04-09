bad3b066f467951b6213cea2a3dc916554a4cd4f
Kimberly Nguyen <kimberlyvnguyen@gmail.com>
2017-04-08 18:38:33 -0700

    assignment-11-albumview: completed following up to last commit which did not contain modified files

--- scripts/album.js
+++ scripts/album.js
@@ -1,72 +1,95 @@
-  // Create Album Object - The Colors by Pablo Picasso
- var albumPicasso = {
-     title: 'The Colors',
-     artist: 'Pablo Picasso',
-     label: 'Cubism',
-     year: '1881',
-     albumArtUrl: 'assets/images/album_covers/01.png',
-     songs: [
-         { title: 'Blue', duration: '4:26' },
-         { title: 'Green', duration: '3:14' },
-         { title: 'Red', duration: '5:01' },
-         { title: 'Pink', duration: '3:21'},
-         { title: 'Magenta', duration: '2:15'}
-     ]
- };
- 
-  // Create Album Object - The Telephone by Guglielmo Marconi
- var albumMarconi = {
-     title: 'The Telephone',
-     artist: 'Guglielmo Marconi',
-     label: 'EM',
-     year: '1909',
-     albumArtUrl: 'assets/images/album_covers/20.png',
-     songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21'},
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15'}
-     ]
- };
+(function () {
+    "use strict";
+    
+    var albumPicasso, albumMarconi, albumGlitch, createSongRow, template,  albumTitle, albumArtist, albumReleaseInfo, albumImage, albumSongList, setCurrentAlbum, albumsArray, i;
 
-// Create a function named createSongRow that generates the song row content
-var createSongRow = function(songNumber, songName, songLength) {
-     var template =
-        '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
-      + '  <td class="song-item-title">' + songName + '</td>'
-      + '  <td class="song-item-duration">' + songLength + '</td>'
-      + '</tr>'
-      ;
- 
-     return template;
- };
+    // Create Album Object - The Colors by Pablo Picasso
+    albumPicasso = {
+        title: 'The Colors',
+        artist: 'Pablo Picasso',
+        label: 'Cubism',
+        year: '1881',
+        albumArtUrl: 'assets/images/album_covers/01.png',
+        songs: [
+            { title: 'Blue', duration: '4:26' },
+            { title: 'Green', duration: '3:14' },
+            { title: 'Red', duration: '5:01' },
+            { title: 'Pink', duration: '3:21'},
+            { title: 'Magenta', duration: '2:15'}
+        ]
+    };
 
-// Create a function named setCurrentAlbum that will take one of our album objects as an argument and will utilize the object's stored information by injecting it into the template
- var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
-     // #2
-     albumTitle.firstChild.nodeValue = album.title;
-     albumArtist.firstChild.nodeValue = album.artist;
-     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-     albumImage.setAttribute('src', album.albumArtUrl);
- 
-     // #3
-     albumSongList.innerHTML = '';
- 
-     // #4
-     for (var i = 0; i < album.songs.length; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
-     }
- };
- 
- window.onload = function() {
-     setCurrentAlbum(albumMarconi);
- };
+    // Create Album Object - The Telephone by Guglielmo Marconi
+    albumMarconi = {
+        title: 'The Telephone',
+        artist: 'Guglielmo Marconi',
+        label: 'EM',
+        year: '1909',
+        albumArtUrl: 'assets/images/album_covers/20.png',
+        songs: [
+            { title: 'Hello, Operator?', duration: '1:01' },
+            { title: 'Ring, ring, ring', duration: '5:01' },
+            { title: 'Fits in your pocket', duration: '3:21'},
+            { title: 'Can you hear me now?', duration: '3:14' },
+            { title: 'Wrong phone number', duration: '2:15'}
+        ]
+    };
+
+    // Create Album Object - Drive It Like You Stole It by The Glitch Mob
+    albumGlitch = {
+        title: 'Drive It Like You Stole It',
+        artist: 'The Glitch Mob',
+        label: 'Self-Released',
+        year: '2011',
+        albumArtUrl: 'assets/images/album_covers/22.png',
+        songs: [
+            { title: 'Drive It Like You Stole It', duration: '5:52' },
+            { title: 'Between Two Points (feat. Swan)', duration: '5:35' }
+        ]
+    };
+
+    // Create a function named createSongRow that generates the song row content
+    createSongRow = function (songNumber, songName, songLength) {
+        template = '<tr class="album-view-song-item">'
+            + '  <td class="song-item-number">' + songNumber + '</td>'
+            + '  <td class="song-item-title">' + songName + '</td>'
+            + '  <td class="song-item-duration">' + songLength + '</td>'
+            + '</tr>';
+        return template;
+    };
+
+    // Create a function named setCurrentAlbum that will take one of our album objects as an argument and will utilize the object's stored information by injecting it into the template
+    
+    albumTitle = document.getElementsByClassName('album-view-title')[0];
+    albumArtist = document.getElementsByClassName('album-view-artist')[0];
+    albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+    albumImage = document.getElementsByClassName('album-cover-art')[0];
+    albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+    setCurrentAlbum = function (album) {
+        
+        albumTitle.firstChild.nodeValue = album.title;
+        albumArtist.firstChild.nodeValue = album.artist;
+        albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+        albumImage.setAttribute('src', album.albumArtUrl);
+        albumSongList.innerHTML = '';
+        
+        for (i = 0; i < album.songs.length; i += 1) {
+            albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+        }
+    };
+
+    window.onload = function () {
+        setCurrentAlbum(albumMarconi);
+        albumsArray = [albumPicasso, albumGlitch, albumMarconi];
+        i = 1;
+        albumImage.addEventListener("click", function (event) {
+            
+            setCurrentAlbum(albumsArray[i]);
+            i += 1;
+            if (i == albumsArray.length) {
+                i = 0;
+            }
+        });
+    };
+}());
