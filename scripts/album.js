123f0bf3c7d7ea7206da45f8042b5de759e60c9c
Kimberly Nguyen <kimberlyvnguyen@gmail.com>
2017-04-29 14:18:28 -0700

    checkpoint-17-jquery-collection-album-views: complete

--- scripts/album.js
+++ scripts/album.js
@@ -1,8 +1,31 @@
 //(function () {
 //    "use strict";
-    
+
 // Define all variables at head
-var albumPicasso, albumMarconi, createSongRow, template, setCurrentAlbum, albumTitle, albumArtist, albumReleaseInfo, albumImage, albumSongList, findParentByClassName, currentParent, getSongItem, songListContainer, playButtonTemplate, pauseButtonTemplate, clickHandler, songRows, i, songItem, songItemNumber, currentlyPlayingSongElement, currentlyPlayingSong;
+var albumPicasso,
+    albumMarconi,
+    createSongRow,
+    template,
+    setCurrentAlbum,
+    $albumTitle,
+    $albumArtist,
+    $albumReleaseInfo,
+    $albumImage,
+    $albumSongList,
+    $newRow,
+    findParentByClassName,
+    currentParent,
+    getSongItem,
+    songListContainer,
+    playButtonTemplate,
+    pauseButtonTemplate,
+    clickHandler,
+    songRows,
+    i,
+    songItem,
+    songItemNumber,
+    currentlyPlayingSongElement,
+    currentlyPlayingSong;
 
 // Create Album Object 01 - The Colors by Pablo Picasso
 albumPicasso = {
@@ -41,46 +64,55 @@ createSongRow = function (songNumber, songName, songLength) {
     template =
         // Row class is 'album-view-song-item', parent class of data items
         '<tr class="album-view-song-item">'
-        // first data item has a class of 'song-item-number', We will store an additional data attribute here called 'data-song-number'. This will allow us to access the data via DOM.
+
+        // First data item has a class of 'song-item-number'
+        // We will store an additional data attribute here called 'data-song-number'.
+        // This will allow us to access the data via DOM.
         + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+
         // Song-item-title is another data in the same row as song-item-number
         + '  <td class="song-item-title">' + songName + '</td>'
+
         // Song-item-duration is another data in the same row as song-item-number
         + '  <td class="song-item-duration">' + songLength + '</td>'
         + '</tr>';
-    return template;
+
+    return $(template);
 };
 
 // Create a function named setCurrentAlbum that will take one of our album objects as an argument
 setCurrentAlbum = function (album) {
-    
-    // Navigate to correct class and assign first child.
-    albumTitle = document.getElementsByClassName('album-view-title')[0];
-    albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    albumImage = document.getElementsByClassName('album-cover-art')[0];
-    albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-    // At the appropriate class, select node value to set equal to album.property
-    albumTitle.firstChild.nodeValue = album.title;
-    albumArtist.firstChild.nodeValue = album.artist;
-    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-    albumImage.setAttribute('src', album.albumArtUrl);
-    albumSongList.innerHTML = '';
-    
-    // Use a script to assign each album.song[index] to its corresponding song number, name, and length.
+    // Assign classes to jQuery objects.
+    $albumTitle = $('.album-view-title');
+    $albumArtist = $('.album-view-artist');
+    $albumReleaseInfo = $('.album-view-release-info');
+    $albumImage = $('.album-cover-art');
+    $albumSongList = $('.album-view-song-list');
+
+    // Assign album detail elements using text and attribute methods
+    $albumTitle.text(album.title);
+    $albumArtist.text(album.artist);
+    $albumReleaseInfo.text(album.year + ' ' + album.label);
+    $albumImage.attr('src', album.albumArtUrl);
+
+    $albumSongList.empty();
+
+    // Assign album.song[index] to its corresponding song#, name, and length.
     for (i = 0; i < album.songs.length; i += 1) {
-        albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+        $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+        $albumSongList.append($newRow);
     }
 };
 
-// Create a function that will look up the DOM tree until a target parent class is found. Be sure to account for null cases. 
+// Create a function that will look up the DOM tree until a target parent class is found.
+// Be sure to account for null cases.
 findParentByClassName = function (currentElement, targetParentClass) {
-    
+
     // If current element and target class are not valid entries then log out 'null'
     if (currentElement === null || currentElement.parentElement === null) {
         console.log("null");
-    
+
     // Otherwise, iterate up until parent class is equivalent to target parent class
     } else {
         currentParent = currentElement.parentElement;
@@ -91,10 +123,10 @@ findParentByClassName = function (currentElement, targetParentClass) {
     }
 };
 
-// Consider possible use cases where an element may need to be related to its song-item number, and how it will get there.
+// Consider possible use cases where an element may need to be related to its song-item number
 getSongItem = function (element) {
     switch (element.className) {
-        
+
     // look up DOM tree for song-item number
     case 'album-song-button':
         return findParentByClassName(element, 'song-item-number');
@@ -129,23 +161,23 @@ songRows = document.getElementsByClassName('album-view-song-item');
 playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 
-// When there is a mouse click, the target of the mouse click will be sent to the clickHandler function. The function checks whether there is a song playing prior to this click and will react in one of two ways. 
+// When there is a mouse click, the target of the mouse click will be sent to the clickHandler function. The function checks whether there is a song playing prior to this click and will react in one of two ways.
 clickHandler = function (targetElement) {
-    
-    // We will act on the song item number inputted 
+
+    // We will act on the song item number inputted
     songItem = getSongItem(targetElement);
-    
+
     // If there is no song currently playing, then render the current play button to a pause button and changing the state of currentlyPlayingSong from null to its data-song-number.
     if (currentlyPlayingSong === null) {
         songItem.innerHTML = pauseButtonTemplate;
         currentlyPlayingSong = songItem.getAttribute('data-song-number');
-        
-    // If the song we clicked on is playing, simply revert the state from 'data-song-number' to 'null' and revert the pause button into a play button. 
+
+    // If the song we clicked on is playing, simply revert the state from 'data-song-number' to 'null' and revert the pause button into a play button.
     } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
         songItem.innerHTML = playButtonTemplate;
         currentlyPlayingSong = null;
-        
-    // If there is a song playing, but it isn't the one we are clicking on, then adjust the pause button of the song item previously playing from the button to its original song number. Then, render a pause button on the current song number of the mouse click target. Finally, change the state of currentlyPlayingSong from the previous song number to this new song number associated with the target of the mouse click. 
+
+    // If there is a song playing, but it isn't the one we are clicking on, then adjust the pause button of the song item previously playing from the button to its original song number. Then, render a pause button on the current song number of the mouse click target. Finally, change the state of currentlyPlayingSong from the previous song number to this new song number associated with the target of the mouse click.
     } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
         currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
         currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
@@ -156,44 +188,44 @@ clickHandler = function (targetElement) {
 
 // Now state what should show upon page load and then define some event listeners
 window.onload = function () {
-    
+
     // Picasso will show for now (static)
     setCurrentAlbum(albumPicasso);
-    
+
     // The initial state of currentlyPlayingSong will be null.
     currentlyPlayingSong = null;
-    
-    // An event listener will listen for when the mouse hovers over the album's song list. 
+
+    // An event listener will listen for when the mouse hovers over the album's song list.
     songListContainer.addEventListener('mouseover', function (event) {
-        
+
         // The target event property returns the element that triggered the event. We will associate the element (target) the mouse is on with its row (the parent class 'album-view-song-item').
         if (event.target.parentElement.className === 'album-view-song-item') {
-            
-            // We then get the song-item associated with this row. 
+
+            // We then get the song-item associated with this row.
             songItem = getSongItem(event.target);
-            
-            // Finally, we grab the song nummber and check that it isn't a song that's currently playing. If that's the case, then we make a play button appear so that the user can click play if they want to :) 
+
+            // Finally, we grab the song nummber and check that it isn't a song that's currently playing. If that's the case, then we make a play button appear so that the user can click play if they want to :)
             if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
                 songItem.innerHTML = playButtonTemplate;
             }
         }
     });
-    
+
     for (i = 0; i < songRows.length; i += 1) {
-        
+
         // Now, in order to make the play button disappear after the mouse leaves, we will add the 'mouseleave' listener.
         songRows[i].addEventListener('mouseleave', function (event) {
-            
-            // We assign songItem to the song-item-number class of the element that triggered the event (target). We grab its actual number by getting the data-song-number attribute. 
+
+            // We assign songItem to the song-item-number class of the element that triggered the event (target). We grab its actual number by getting the data-song-number attribute.
             songItem = getSongItem(event.target);
             songItemNumber = songItem.getAttribute('data-song-number');
-            
+
             // If the song isn't currently playing, then we revert it to its song number image by changing the innerHTML from 'playButtonTemplate' to it's original 'songItemNumber'.
             if (songItemNumber !== currentlyPlayingSong) {
                 songItem.innerHTML = songItemNumber;
             }
         });
-        
+
         // When there is a mouse click, the target of the mouse click will be sent to the clickHandler function. The function checks whether there is a song playing and whether the song we clicked on is the song that is playing.
         songRows[i].addEventListener('click', function (event) {
             clickHandler(event.target);
--- scripts/collection.js
+++ scripts/collection.js
@@ -1,29 +1,44 @@
-(function () {
-    "use strict";
-    
-    var collectionItemTemplate, collectionContainer, i;
-    
-    collectionItemTemplate =
-        '<div class="collection-album-container column fourth">'
-        + '  <img src="assets/images/album_covers/01.png"/>'
-        + '  <div class="collection-album-info caption">'
-        + '    <p>'
-        + '      <a class="album-name" href="album.html"> The Colors </a>'
-        + '      <br/>'
-        + '      <a href="album.html"> Pablo Picasso </a>'
-        + '      <br/>'
-        + '      X songs'
-        + '      <br/>'
-        + '    </p>'
-        + '  </div>'
-        + '</div>';
+var buildCollectionItemTemplate, template, $collectionContainer, i;
 
-    window.onload = function () {
-        collectionContainer = document.getElementsByClassName('album-covers')[0];
-        collectionContainer.innerHTML = '';
-        for (i = 0; i < 12; i += 1) {
-            collectionContainer.innerHTML += collectionItemTemplate;
-        }
-    };
-    
-}());
+// Wrap template in a function toreturn markup string as jQuery object.
+// When naming action-oriented functions, start with a verb.
+buildCollectionItemTemplate = function() {
+
+  // Store collectionItemTemplate to template
+  template =
+    '<div class = "collection-album-container column fourth">'
+  + ' <img src = "assets/images/album_covers/01.png"/>'
+  + ' <div class = "collection-album-info caption">'
+  + '    <p>'
+  + '      <a class="album-name" href="album.html"> The Colors </a>'
+  + '      <br/>'
+  + '      <a href="album.html"> Pablo Picasso </a>'
+  + '      <br/>'
+  + '      X songs'
+  + '      <br/>'
+  + '    </p>'
+  + '  </div>'
+  + '</div>';
+
+  // Wrap template in a jQuery object
+  return $(template);
+};
+
+// Refactoring: $(window).load() is jQuery equivalent of window.onload
+$(window).load(function() {
+
+  // Refactoring: shorter jQuery alternative to DOM selection
+  // '$' used to indicate jQuery-related variable
+  $collectionContainer = $('.album-covers');
+
+  // Empty() method removes all elements from the elements it's called on.
+  $collectionContainer.empty();
+
+  // To display our 12 albums, we will use the same for loop
+  for (i = 0; i < 12; i += 1) {
+
+    // However, we will use append method rather than +=.
+    var $newThumbnail = buildCollectionItemTemplate();
+    $collectionContainer.append($newThumbnail);
+  }
+});
